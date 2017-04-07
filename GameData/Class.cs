904dbd3f15058959fa40bc74f8a65e78e1ca7bed
ZZerker <ZZerker@gmail.com>
2017-04-07 23:40:30 +0200

    Created Class

---
+++ GameData/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameData
+{/*#class_ID;   realm;  class_name; magic;  stat1;  stat2;  stat3;  skillfactor;    basehp; hpfac;  races;  race1;  race2;  race3;  race4;  race5;  styles; style1; style2; style3; style4; style5; style6; style7; spells; spell1; spell2; spell3;
+    0           1       2           3       4       5       6       7               8       9       10      11      12      13      14      15      16      17      18      19      20      21      22      23      24      25      26      27
+    1;alb;Infiltrator;0; dex; qui; str; 2.5;740; 3.6;3;2;13;4;;;5; 27*;1;2;22;33;;;1;4;;;*/
+    public class Class
+    {
+        public string ClassId { get; set; }
+
+        public Realm Realm { get; set; }
+
+        public string Name { get; set; }
+
+
+
+    }
+}
